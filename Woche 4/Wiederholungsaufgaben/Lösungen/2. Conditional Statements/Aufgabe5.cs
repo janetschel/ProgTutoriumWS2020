0766ab1a4f1eab7caf14344c7dd32a1290b3b26e
Jan Etschel <46886724+janetschel@users.noreply.github.com>
2020-11-15 18:50:11 +0100

    Wiederholungsaufgaben zur Vertiefung

---
+++ Woche 4/Wiederholungsaufgaben/Lösungen/2. Conditional Statements/Aufgabe5.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Conditional_Statements
+{
+    class Aufgabe5
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Wie lautet deine erste Zahl?: ");
+            var number1 = Convert.ToInt32(Console.ReadLine());
+            
+            Console.Write("Wie lautet dein Operator?: ");
+            var operatorOfCalculation = Console.ReadLine();
+            
+            Console.Write("Wie lautet deine zweite Zahl?: ");
+            var number2 = Convert.ToInt32(Console.ReadLine());
+
+
+            int result = 0;
+            switch (operatorOfCalculation)
+            {
+                case "+":
+                    result = number1 + number2;
+                    break;
+                case "-":
+                    result = number1 - number2;
+                    break;
+                case "*":
+                    result = number1 * number2;
+                    break;
+                case "/":
+                    result = number1 / number2;
+                    break;
+                default:
+                    Console.WriteLine($"Unglültiger Operator");
+                    break;
+            }
+            
+            Console.WriteLine($"Dein Ergebnis ist {result}");
+        }
+    }
+}
