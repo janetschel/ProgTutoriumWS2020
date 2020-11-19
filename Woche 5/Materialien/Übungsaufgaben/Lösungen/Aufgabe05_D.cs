29515aae1217dcd921f0fdab0540659cb3f70fc6
Jan Etschel <46886724+janetschel@users.noreply.github.com>
2020-11-19 12:10:57 +0100

    Bessere Version der Aufgabe

--- Woche 5/Materialien/Übungsaufgaben/Lösungen/Aufgabe05_D.cs
+++ Woche 5/Materialien/Übungsaufgaben/Lösungen/Aufgabe05_D.cs
@@ -15,14 +15,12 @@ namespace Arrays
 
         static int[] FindSumInArray(int[] array, int sum)
         {
-            int[] indices = {-1, -1}; // falls kein Paar bekannt ist, geben wir als Fehlerzustand den Index -1 zurück
-
             for (var i = 0; i < array.Length; i++)
                 for (var j = i + 1; j < array.Length; j++)
                     if (array[i] + array[j] == sum)
                         return new[] {i, j};
             
-            return indices;
+            return {-1, -1}; // falls kein Paar bekannt ist, geben wir als Fehlerzustand den Index -1 zurück
         }
     }
 }
