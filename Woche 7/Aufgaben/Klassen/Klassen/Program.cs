140a591a9142f15448e08980197176c5219e49e5
Jan Etschel <46886724+janetschel@users.noreply.github.com>
2020-12-03 17:22:51 +0100

    Exceptions und Klassen

---
+++ Woche 7/Aufgaben/Klassen/Klassen/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Klassen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student jan = new Student();
+            
+            jan.Vorname = "Jan";
+            jan.Nachname = "Etschel";
+            
+            try
+            {
+                jan.Manr = -1;   
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Fehler beim Setzen von Manr: " + e.Message);
+                return;
+            }
+            
+            jan.SayHello();
+        }
+    }
+
+    class Student
+    {
+        private string vorname;
+        private string nachname;
+        private int manr;
+
+        public string Vorname
+        {
+            get { return vorname;  }
+            set { vorname = value; }
+        }
+        
+        public string Nachname
+        {
+            get { return vorname;  }
+            set { vorname = value; }
+        }
+
+        public int Manr
+        {
+            get { return manr; }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new Exception("Manr nur positiv bitte!");
+                }
+
+                manr = value;
+            }
+        }
+
+        public void SayHello()
+        {
+            Console.WriteLine($"Hallo, ich bin {vorname}");
+            Console.WriteLine($"Meine Manr ist {Manr}");
+        }
+    }
+}
