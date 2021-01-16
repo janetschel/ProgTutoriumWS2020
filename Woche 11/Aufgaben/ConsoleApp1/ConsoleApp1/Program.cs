646cbe327d0f814f188438808427d6542bc7a3a7
Jan Etschel <46886724+janetschel@users.noreply.github.com>
2021-01-16 11:14:11 +0100

    Fragestunden

---
+++ Woche 11/Aufgaben/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var b = new Bruch();
+        }
+    }
+
+    class Bruch
+    {
+        private int nenner;
+
+        public int Nenner
+        {
+            get => nenner;
+            set => nenner = value;
+        }
+
+        public Bruch(int n)
+        {
+            Nenner = n;
+            nenner = n;
+        }
+    }
+}
---
+++ Woche 11/Materialien/Fragestunde/Fragestunde/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+
+namespace Fragestunde
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region StreamReader
+            StreamReader sr = new StreamReader("test.txt");
+            string line = sr.ReadLine();
+
+            while (line != null)
+            {
+                if (line == "Hi")
+                {
+                    Console.WriteLine("Hi Welt");
+                }
+
+                Console.WriteLine(line);
+
+                // Am Ende: Neue Zeile lesen:
+                line = sr.ReadLine();
+            }
+            sr.Close();
+            #endregion
+
+            #region StreamWriter
+            StreamWriter sw = new StreamWriter("test2.txt");
+            
+            sw.WriteLine("Hallo");
+            sw.WriteLine("Welt");
+            sw.WriteLine("!");
+            
+            sw.Flush();
+            sw.Close();
+            #endregion
+        }
+
+        #region Rekursion
+        static int FakultaetRekursiv(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return n * FakultaetRekursiv(n - 1);
+            }
+        }
+
+        static int Fakultaet(int n)
+        {
+            int summe = 1;
+
+            // Iterativ
+            for (int i = 1; i <= n; i++)
+            {
+                summe *= i;
+            }
+
+            return summe;
+        }
+        #endregion
+    }
+}
